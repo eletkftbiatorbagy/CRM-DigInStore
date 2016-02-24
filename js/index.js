16c31c923a8700d0b2145913ba8d8af328ba543b
Attila Kiss <attila.kiss@e-let.hu>
2016-02-24 21:40:01 +0100

    ikonok, menűk, fotózás

--- js/index.js
+++ js/index.js
@@ -16,7 +16,50 @@ var app = 	{
 				}	
     		};
 
+var OLDAL = 0;
+var Oldal_cache = [];
+var UGYFEL;
 
+function login()
+{
+								login_siker();   // ide majd callback jön
+}
+
+function login_siker()
+{
+	oldal(0);
+}
+
+
+function oldal(NR,vissza)
+{
+	var oldalak = document.getElementsByTagName("oldal");
+	for (var o=0; o<oldalak.length; o++)
+	{
+		oldalak[o].style.display = (oldalak[o].id==NR)?"block":"none";
+	}
+	if (!vissza) { Oldal_cache.push(NR); }
+	OLDAL = NR;
+	if (!UGYFEL && OLDAL<100 && !vissza) { oldal(100); }
+}
+
+function oldal_object(NR)
+{
+	var oldalak = document.getElementsByTagName("oldal");
+	var obj;
+	for (var o=0; o<oldalak.length; o++)
+	{
+		if (oldalak(o).id==NR) { obj = oldalak(o); }
+	}
+	return obj;
+}
+
+function vissza()
+{
+	Oldal_cache.pop();
+	var elozo_oldal = Oldal_cache[Oldal_cache.length-1];
+	oldal(elozo_oldal,true);
+}
 
 function scan(){
 	cordova.plugins.barcodeScanner.scan(
@@ -33,3 +76,29 @@ function scan(){
 	
 }
 
+
+function kivalaszt()
+{
+	UGYFEL = 1;
+	vissza();
+	vissza();
+}
+
+
+function uj_foto(ablak)
+{
+	var foto_kesz = function(imageData) {
+							var img = document.createElement("img");
+								img.src = imageData;
+								img.className = "foto";
+							oldal_object(ablak).getElementsByTagName("main")[0].appendChild(img);		
+						};
+
+	setTimeout(function() {
+    	navigator.camera.getPicture(foto_kesz, hiba, { quality: 75,
+    						destinationType: Camera.DestinationType.DATA_URL,
+    						encodingType: Camera.EncodingType.JPEG,
+    						saveToPhotoAlbum: false
+		});
+	}, 0);
+}
